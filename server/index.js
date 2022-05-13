272db85f7cf0d3a98372f2f9f9f238875fe581e2
Mirano Tanaka <m_tanaka@axseed.co.jp>
2022-05-13 18:04:23 +0900

    change port at index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const compression = require('compression');
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8000;
 // Gzip
 app.use(compression());
 // Run the app by serving the static files
