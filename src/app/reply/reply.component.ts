c6e90b6cbc2500173d49868f9154ef2409ef1f03
Mirano Tanaka <m_tanaka@axseed.co.jp>
2022-05-23 17:38:25 +0900

    reply ts

--- src/app/reply/reply.component.ts
+++ src/app/reply/reply.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReplyComponent implements OnInit {
 
+  r_button:Number=0;
+  good_num:number=0;
+  submit_r:Number=0;
+  reply_com:String="返信コメント";
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  reply(): void {
+    this.r_button=1;
+    this.submit_r=0;
+  }
+
+  good(): void {
+    this.good_num=this.good_num+1;
+  }
+
+  do_reply():void{
+    this.submit_r=1;
+  }
+
+  delete(): void {
+    this.good_num=this.good_num+1;
+  }
+
 
 }
